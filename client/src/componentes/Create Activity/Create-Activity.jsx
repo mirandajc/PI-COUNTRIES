7ab1169412973add539ef06196ddcb95ef190af5
mirandajc <74868639+mirandajc@users.noreply.github.com>
2022-05-23 16:43:12 -0300

    components creados

---
+++ client/src/componentes/Create Activity/Create-Activity.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreateActivity() {
+    return (
+        <div>
+            <h1>CreateActivity</h1>
+        </div>
+    )
+}
+
+export default CreateActivity;
